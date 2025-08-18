58eb436bf0228c75d2534c5ae3976be36bc7a3de
Kimberlee97 <93462505+Kimberlee97@users.noreply.github.com>
2025-08-18 17:37:20 +0800

    Add Add and List features

--- src/main/java/Lumi.java
+++ src/main/java/Lumi.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Lumi {
     private static final String LOGO = "LUMI (˶ˆᗜˆ˵)";
+    private static List<String> list = new ArrayList<>();
 
     /** Prints a greeting */
     public static void greet() {
@@ -21,8 +24,14 @@ public class Lumi {
             if (input.equals("bye")) {
                 bye();
                 break;
+            } else if (input.equals("list")){
+                for (int i = 0; i < list.size(); i++) {
+                    int index = i + 1;
+                    System.out.println(index + ". " + list.get(i));
+                }
             } else {
-                System.out.println(input);
+                list.add(input);
+                System.out.println("added: " + input);
             }
         }
         scanner.close();
