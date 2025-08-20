80d56f02642c47f0ae673d485dbe8b47bbc19b28
Kimberlee97 <93462505+Kimberlee97@users.noreply.github.com>
2025-08-21 00:22:06 +0800

    Add error handling
    Invalid tasks would trigger IllegalArgumentExceptions, invalid inputs for mark and unmark would trigger IndexOutOfBoundsExcpetion and NumberFormatException, and a task could be marked despite already being marked and unmarked despite already being unmarked.
    Let's handle the errors for these cases.

--- src/main/java/Lumi.java
+++ src/main/java/Lumi.java
@@ -17,39 +17,70 @@ public class Lumi {
         private final String desc;
         private final String type;
 
-        public Task(String desc) {
+        public Task(String desc) throws IllegalArgumentException{
             this.isDone = false;
+            String tempType = "";
+            String tempDesc = "";
             String[] taskDesc = desc.split(" ", 2);
+            if (taskDesc.length < 2) throw new IllegalArgumentException("Please add a task in the format:" +
+                    "todo <task>\n" +
+                    "deadline <task> /by <deadline>\n" +
+                    "event <task> /from <date/time> /to <date/time>");
             String taskType = taskDesc[0];
 
             switch (taskType) {
+                // adds a todo task
                 case "todo":
-                    this.type = TODO;
-                    this.desc = taskDesc[1];
+                    tempType = TODO;
+                    if (taskDesc[1].trim().isEmpty()) throw new IllegalArgumentException("Please add a todo task in " +
+                            "the format:\n todo <task> (task should not be empty :> )");
+                    tempDesc = taskDesc[1];
                     break;
+                // adds a deadline task
                 case "deadline":
-                    this.type = DEADLINE;
+                    tempType = DEADLINE;
                     String[] deadlineParts = taskDesc[1].split("/by ");
-                    this.desc = deadlineParts[0] + "(by: " + deadlineParts[1] + ")";
+                    if (deadlineParts.length < 2 || deadlineParts[0].trim().isEmpty() ||
+                            deadlineParts[1].trim().isEmpty()) {
+                        throw new IllegalArgumentException("Please enter a deadline task " +
+                                "in the format: deadline <task> /by <deadline>");
+                    }
+                    tempDesc = deadlineParts[0] + "(by: " + deadlineParts[1] + ")";
                     break;
+                // adds an event
                 case "event":
-                    this.type = EVENT;
+                    tempType = EVENT;
                     String[] eventParts = taskDesc[1].split("/from |/to ");
-                    this.desc = eventParts[0] + "(from: " + eventParts[1] + " to: " + eventParts[2] + ")";
+                    if (eventParts.length < 3 || eventParts[0].trim().isEmpty() || eventParts[1].trim().isEmpty() ||
+                            eventParts[2].trim().isEmpty()) {
+                        throw new IllegalArgumentException("Please enter an event task in the " +
+                                "format: event <task> /from <date/time> /to <date/time>");
+                    }
+                    tempDesc = eventParts[0] + "(from: " + eventParts[1] + " to: " + eventParts[2] + ")";
                     break;
                 default:
-                    throw new IllegalArgumentException("Invalid task type");
+                    throw new IllegalArgumentException("Oh no! >.< \n I'm not sure what this is, please try again!");
             }
+            this.type = tempType;
+            this.desc = tempDesc;
         }
 
         /** Marks task as done */
         public void mark() {
+            if (this.isDone) {
+                System.out.println("This task has already been marked as done ՞. .՞");
+                return;
+            }
             this.isDone = true;
             System.out.println("Yay! I have marked this task as done: " + this.desc);
         }
 
         /** Marks task as undone */
         public void unmark() {
+            if (!this.isDone) {
+                System.out.println("This task has already been marked undone ՞. .՞");
+                return;
+            }
             this.isDone = false;
             System.out.println("Oki, I've marked this task as undone: " + this.desc);
         }
@@ -98,6 +129,7 @@ public class Lumi {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
+            if (input.trim().isEmpty()) continue;
             if (input.equals("bye")) {
                 bot.bye();
                 break;
@@ -118,6 +150,8 @@ public class Lumi {
                     }
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("Please enter a valid task number");
+                } catch (NumberFormatException e) {
+                    System.out.println("Please enter a number after 'mark'/'unmark'");
                 }
             } else {
                 bot.add(input);
