1aba625e3acb5ea9cef3b58af55eeef474e1384b
Kimberlee97 <93462505+Kimberlee97@users.noreply.github.com>
2025-08-20 13:00:44 +0800

    Replace String[] with Task class

--- src/main/java/Lumi.java
+++ src/main/java/Lumi.java
@@ -4,50 +4,87 @@ import java.util.ArrayList;
 
 public class Lumi {
     private static final String LOGO = "LUMI (˶ˆᗜˆ˵)";
-    private static List<String[]> list = new ArrayList<>();
+    private static final String DONE = "[X]";
+    private static final String UNDONE = "[ ]";
+
+    private static List<Task> list = new ArrayList<>();
+
+    class Task {
+        private boolean isDone;
+        private final String desc;
+
+        public Task(String desc) {
+            this.desc = desc;
+            this.isDone = false;
+        }
+
+        /** Marks task as done */
+        public void mark() {
+            this.isDone = true;
+            System.out.println("Nice! I have marked this task as done: " + this.desc);
+        }
+
+        /** Marks task as undone */
+        public void unmark() {
+            this.isDone = false;
+            System.out.println("Oki, I've marked this task as undone: " + this.desc);
+        }
+
+        @Override
+        public String toString() {
+            return (isDone ? DONE : UNDONE) + " " + this.desc;
+        }
+    }
 
     /** Prints a greeting */
-    public static void greet() {
+    private void greet() {
         System.out.println("Hello from\n" + LOGO + "\nWhat can I do for you?");
     }
 
     /** Prints a goodbye message */
-    public static void bye() {
+    private void bye() {
         System.out.println("Bye! See you next time :>");
     }
 
+    /** Adds a new item */
+    private void add(String input) {
+        Task newTask = new Task(input);
+        list.add(newTask);
+        System.out.println("added: " + input);
+    }
+
+    /** Prints out the list */
+    private static void printList() {
+        if (list.isEmpty()) {
+            System.out.println("No items yet");
+        }
+        for (int i = 0; i < list.size(); i++) {
+            int index = i + 1;
+            System.out.println(index + ". " + list.get(i).toString());
+        }
+    }
+
     public static void main(String[] args) {
-        greet();
+        Lumi bot = new Lumi();
+        bot.greet();
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
-                bye();
+                bot.bye();
                 break;
             } else if (input.equals("list")) {
-                if (list.isEmpty()) {
-                    System.out.println("No items yet");
-                }
-                for (int i = 0; i < list.size(); i++) {
-                    int index = i + 1;
-                    System.out.println(index + ". [" + list.get(i)[0] + "] " + list.get(i)[1]);
-                }
-            } else if (input.startsWith("unmark")) {
+                printList();
+            } else if (input.startsWith("unmark") || input.startsWith("mark")) {
                 String[] parts = input.split(" ");
                 int index = Integer.parseInt(parts[1]) - 1;
-                list.set(index, new String[]{" ", list.get(index)[1]});
-                System.out.println("Oki! I've marked this task as undone: ");
-                System.out.println(list.get(index)[1]);
-            } else if (input.startsWith("mark")) {
-                String[] parts = input.split(" ");
-                int index = Integer.parseInt(parts[1]) - 1;
-                list.set(index, new String[]{"X", list.get(index)[1]});
-                System.out.println("Nice! I've marked this task as done: ");
-                System.out.println(list.get(index)[1]);
+                if (parts[0].equals("unmark")) {
+                    list.get(index).unmark();
+                } else {
+                    list.get(index).mark();
+                }
             } else {
-                String[] newItem = new String[]{" ", input};
-                list.add(newItem);
-                System.out.println("added: " + input);
+                bot.add(input);
             }
         }
         scanner.close();
