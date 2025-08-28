cf66fb80d50018e577917ffef38482d29696a6fe
Kimberlee97 <93462505+Kimberlee97@users.noreply.github.com>
2025-08-29 03:23:05 +0800

    Refactor to apply a more OOP structure

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,4 +1,4 @@
-import exceptions.InvalidTaskException;
+import exceptions.LumiException;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
@@ -6,12 +6,12 @@ import java.time.format.DateTimeParseException;
 public class Event extends Task{
     private final String desc;
 
-    public Event(String desc) throws InvalidTaskException {
+    public Event(String desc) throws LumiException {
         super(TaskType.EVENT);
         String[] eventParts = desc.split("/from|/to|\\|From: |\\|To: ");
         if (eventParts.length < 3 || eventParts[0].trim().isEmpty() || eventParts[1].trim().isEmpty()
                 || eventParts[2].trim().isEmpty()) {
-            throw new InvalidTaskException("Please enter an event task in the "
+            throw new LumiException("Please enter an event task in the "
                     + "format: event <task> /from <date/time> /to <date/time>");
         } else {
             try {
@@ -20,29 +20,9 @@ public class Event extends Task{
                 this.desc = eventParts[0] + "|From: " + DateTimeParser.format(from) + "|To: "
                         + DateTimeParser.format(to);
             } catch (DateTimeParseException e) {
-                throw new InvalidTaskException(e.getMessage());
+                throw new LumiException(e.getMessage());
             }
         }
-    }
-
-    @Override
-    public void mark() {
-        if (super.getIsDone()) {
-            System.out.println("This task has already been marked as done ՞. .՞");
-            return;
-        }
-        super.mark();
-        System.out.println("Yay! I have marked this task as done: " + this.desc);
-    }
-
-    @Override
-    public void unmark() {
-        if (!this.getIsDone()) {
-            System.out.println("This task has already been marked undone ՞. .՞");
-            return;
-        }
-        super.unmark();
-        System.out.println("Oki, I've marked this task as undone: " + this.desc);
     }
 
     @Override
